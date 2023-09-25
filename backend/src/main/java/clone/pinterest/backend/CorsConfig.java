4503b7b77f008010288d427a3063001e7d97dc5d
solban1 <solban1@daum.net>
2023-09-25 17:47:00 +0900

    Add CORS config

---
+++ backend/src/main/java/clone/pinterest/backend/CorsConfig.java
@@ -0,0 +1,21 @@
+package clone.pinterest.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public RepositoryRestConfigurer corsConfigurer() {
+        return new RepositoryRestConfigurer() {
+            @Override
+            public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+                cors.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
