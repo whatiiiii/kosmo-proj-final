bd9a80c7a88f9e621d8bbcdf1e3cd7c8e2172846
Evergreen3891 <songrok11@gmail.com>
2023-09-27 10:30:08 +0900

    add makepin

---
+++ frontend/src/components/MakePin.tsx
@@ -0,0 +1,17 @@
+import PinNavBar from "./PinNavBar";
+
+function MakePin() {
+  return (
+    <>
+      <PinNavBar />
+      <div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+}
+const styles: Record<string, React.CSSProperties> = {};
+
+export default MakePin;
