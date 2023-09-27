440f0526bf6f5c7a8a6063909c411bee470620dc
Evergreen3891 <songrok11@gmail.com>
2023-09-27 16:48:10 +0900

    makepin 추가

--- frontend/src/components/MakePin.tsx
+++ frontend/src/components/MakePin.tsx
@@ -42,17 +42,6 @@ function MakePin() {
     </>
   );
 }
-function setThumbnail(event) {
-  var reader = new FileReader();
-
-  reader.onload = function (event) {
-    var img = document.createElement("img");
-    img.setAttribute("src", event.target.result);
-    document.querySelector("div#image_container").appendChild(img);
-  };
-
-  reader.readAsDataURL(event.target.files[0]);
-}
 const styles: Record<string, React.CSSProperties> = {
   mainscreen: {
     paddingTop: 55.972,
