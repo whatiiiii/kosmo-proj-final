93ba760a0c2f6b57e1f91af2e8323122c3b48b8e
solban1 <solban1@daum.net>
2023-10-13 15:46:12 +0900

    Add findAllIds to PinRepository

--- backend/src/main/java/clone/pinterest/backend/repository/PinRepository.java
+++ backend/src/main/java/clone/pinterest/backend/repository/PinRepository.java
@@ -1,8 +1,10 @@
 package clone.pinterest.backend.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import clone.pinterest.backend.domain.Pin;
 
 public interface PinRepository extends CrudRepository<Pin, Integer> {
-
+    @Query("select p.pinSeq from #{#entityName} p")
+    Integer[] findAllIds();
 }
