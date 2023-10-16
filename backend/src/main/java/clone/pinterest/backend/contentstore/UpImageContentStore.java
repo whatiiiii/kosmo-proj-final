9fd3bbc39ac558663e41f7cbb264d1a26f26f368
solban1 <solban1@daum.net>
2023-10-16 18:32:44 +0900

    Allow more methods on ContentStore

--- backend/src/main/java/clone/pinterest/backend/contentstore/UpImageContentStore.java
+++ backend/src/main/java/clone/pinterest/backend/contentstore/UpImageContentStore.java
@@ -3,11 +3,13 @@ package clone.pinterest.backend.contentstore;
 import org.springframework.content.commons.store.ContentStore;
 import org.springframework.content.rest.StoreRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import clone.pinterest.backend.domain.UpImage;
 
 @StoreRestResource
-@CrossOrigin
+@CrossOrigin(methods = { RequestMethod.GET, RequestMethod.PUT, RequestMethod.POST, RequestMethod.DELETE,
+        RequestMethod.OPTIONS })
 public interface UpImageContentStore extends ContentStore<UpImage, String> {
 
 }
