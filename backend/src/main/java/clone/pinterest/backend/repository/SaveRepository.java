912fdf92eb25e4584ca8a2e91b168efd53f9e49a
solban1 <solban1@daum.net>
2023-10-23 12:49:05 +0900

    Add created, saved pins list in profile page

--- backend/src/main/java/clone/pinterest/backend/repository/SaveRepository.java
+++ backend/src/main/java/clone/pinterest/backend/repository/SaveRepository.java
@@ -1,5 +1,6 @@
 package clone.pinterest.backend.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,4 +12,7 @@ import clone.pinterest.backend.domain.SaveId;
 public interface SaveRepository extends CrudRepository<Save, SaveId> {
     boolean existsByIdMemberIdAndIdPinPinSeq(@Param("memberId") String idMemberId,
             @Param("pinId") Integer idPinId);
+
+    @Query("select s.id.pin.pinSeq from #{#entityName} s where s.id.member.id = ?1 and s.id.pin.pinSeq not in (select p.pinSeq from Pin p where p.writer.id = ?1)")
+    Integer[] findSavedPinIdsByMemberId(@Param("id") String id);
 }
