050ebcb270df616556aecc43f1dae006367cc085
solban1 <solban1@daum.net>
2023-10-25 13:03:53 +0900

    Adjust WelcomePage

--- frontend/src/components/WelcomePage.tsx
+++ frontend/src/components/WelcomePage.tsx
@@ -20,13 +20,21 @@ export default function WelcomePage() {
           alignContent="center"
           justifyContent="center"
           height="calc(100vh - 64px)"
-          sx={{ scrollSnapAlign: "end" }}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers&1)",
+            backgroundSize: "cover",
+            scrollSnapAlign: "end",
+          }}
         >
           <Grid item>
             <Typography
               color="primary"
               variant="h2"
-              sx={{ fontWeight: "bold" }}
+              sx={{
+                fontWeight: "bold",
+                textShadow: "0 0 1em skyblue, 0 0 0.2em skyblue",
+              }}
             >
               좋아하는 아이디어를 저장하세요.
             </Typography>
@@ -37,13 +45,21 @@ export default function WelcomePage() {
           alignContent="center"
           justifyContent="center"
           height="calc(100vh - 64px)"
-          sx={{ scrollSnapAlign: "end" }}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers&2)",
+            backgroundSize: "cover",
+            scrollSnapAlign: "end",
+          }}
         >
           <Grid item>
             <Typography
               color="primary"
               variant="h2"
-              sx={{ fontWeight: "bold" }}
+              sx={{
+                fontWeight: "bold",
+                textShadow: "0 0 1em skyblue, 0 0 0.2em skyblue",
+              }}
             >
               아이디어를 검색하세요.
             </Typography>
@@ -56,12 +72,16 @@ export default function WelcomePage() {
         >
           <Grid
             item
+            container
+            alignContent="center"
+            justifyContent="center"
             xs={false}
             sm={4}
             md={7}
             sx={{
               backgroundImage:
-                "url(https://source.unsplash.com/random?wallpapers)",
+                "url(https://source.unsplash.com/random?wallpapers&3)",
+
               backgroundRepeat: "no-repeat",
               backgroundColor: (t) =>
                 t.palette.mode === "light"
@@ -70,7 +90,20 @@ export default function WelcomePage() {
               backgroundSize: "cover",
               backgroundPosition: "center",
             }}
-          />
+          >
+            <Grid item>
+              <Typography
+                color="white"
+                variant="h2"
+                sx={{
+                  fontWeight: "bold",
+                  textShadow: "0 0 1em darkred, 0 0 0.2em darkred",
+                }}
+              >
+                가입하여 더 많은 아이디어를 만나보세요.
+              </Typography>
+            </Grid>
+          </Grid>
           <Grid
             item
             xs={12}
